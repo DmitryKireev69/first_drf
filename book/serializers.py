2a17676879fccdf38681de443319cb1ab1a3b270
user <user@mail.com>
2025-04-14 17:36:49 +0300

    Доработки по валидации

--- book/serializers.py
+++ book/serializers.py
@@ -4,9 +4,29 @@ from rest_framework import serializers
 from book.models import Book
 
 
+class One_To_Five:
+    """Валидатор на основе класса"""
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, value):
+        if not self.min_value <= value <= self.max_value:
+            message = f'This field must be a between {self.min_value} and {self.max_value}.'
+            raise serializers.ValidationError(message)
+
+
+def one_to_five(value):
+    """Валидатор"""
+    if not 0 <= value <= 5:
+        raise serializers.ValidationError('Рейтинг должен быть от 0 до 5')
+
+
 class BookSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200, default='Django REST Framework')
     author = serializers.CharField(max_length=100, default='kir')
+    ratings = serializers.IntegerField(validators=[one_to_five])
+    ratings1 = serializers.IntegerField(validators=[One_To_Five(0, 5)])
 
     def create(self, validated_data):
         return Book.objects.create(**validated_data)
@@ -16,6 +36,18 @@ class BookSerializer(serializers.Serializer):
         instance.author = validated_data.get('author', instance.author)
         instance.save()
         return instance
+
+    def validate_title(self, value):
+        """Валидация на уровне поля"""
+        if '1' in value:
+            raise serializers.ValidationError('В title не может быть цыфры 1')
+        return value
+
+    def validate(self, data):
+        """Валидация на уровне обьекта"""
+        if data['title'] == data['author']:
+            raise serializers.ValidationError("Введены одинаковые значения полей")
+        return data
 
 
 class LoginSerializer(serializers.Serializer):
